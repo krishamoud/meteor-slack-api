8a547e59b277d33b519e49b1f1db4a09ffd39358
Kris Hamoud <khamoud@umich.edu>
2015-07-10 10:43:13 -0700

    updated package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'khamoud:slack-api',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Slack API written for meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/krishamoud/meteor-slack-api.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
