fb432615b0158ba6640f278aae0039825bd58ba2
Kris Hamoud <kris.hamoud@gmail.com>
2016-02-19 16:33:11 -0800

    updated

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'khamoud:slack-api',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Slack API written for meteor',
   // URL to the Git repository containing the source code for this package.
