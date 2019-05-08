0603fd3aad32701958d7656be10eca7d0fc0c61f
Joe Berry <joeb@homebrewagency.com>
2019-05-08 15:18:32 -0700

    Revert "Update package.js"
    This reverts commit f1153c35790cb1f95ee8d06cab3a616cc8551cb1.

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
-  name: 'joephuz:slack-api',
+  name: 'khamoud:slack-api',
   version: '0.0.2',
   // Brief, one-line summary of the package.
-  //forked from https://github.com/krishamoud/meteor-slack-api
   summary: 'Slack API written for meteor',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/joephuz/meteor-slack-api.git',
+  git: 'https://github.com/krishamoud/meteor-slack-api.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -20,6 +19,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('joephuz:slack-api');
+  api.use('khamoud:slack-api');
   api.addFiles('slack-api-tests.js', ['server']);
 });
